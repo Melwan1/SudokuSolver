9628be65026a25ba031b81afa3965999a73cc036
Melwan Chevassus <melwan.chevassus@epita.fr>
2024-02-22 12:30:14 +0100

    SudokuSolver: add file main.cpp

--- SudokuSolver/main.cpp
+++ SudokuSolver/main.cpp
@@ -2,13 +2,29 @@
 //  main.cpp
 //  SudokuSolver
 //
-//  Created by Christophe Chevassus on 22/02/2024.
+//  Created by Melwan Chevassus on 22/02/2024.
 //
 
 #include <iostream>
+using namespace std;
+
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    cout << "====================================\n";
+    cout << "Welcome to Sudoku Solver\n";
+    cout << "====================================\n";
+    cout << "Grid types:\n";
+    cout << "-----------------------\n";
+    int numberGridTypes = 1;
+    cout << "1 - Normal square grid\n";
+    cout << "-----------------------\n";
+    cout << "Enter the grid type (from 1 to " << numberGridTypes << "): ";
+    int gridType = 0;
+    cin >> gridType;
+    if (gridType <= 0 || gridType > numberGridTypes) {
+        cout << "Entered grid type is invalid!\n";
+        return 1;
+    }
     return 0;
+    
 }
